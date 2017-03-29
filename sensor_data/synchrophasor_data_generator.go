d2c28fa124abe9237b3620da85940e0fcf015863
michael dye <m-github@divisive.info>
2017-03-28 23:11:20 -0600

    added SERIAL env instead of DEVICE_ID (since this will be standalone); also increased throughput 100 fold

--- sensor_data/synchrophasor_data_generator.go
+++ sensor_data/synchrophasor_data_generator.go
@@ -50,7 +50,7 @@ func NewSimpleSynchroDatumGenerator(deviceID string) <-chan SimpleTsDatum {
 			}
 
 			writer <- msg
-			time.Sleep(100 * time.Millisecond)
+			time.Sleep(1 * time.Millisecond)
 		}
 	}()
 
